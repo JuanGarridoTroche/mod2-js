262801b4cfddd4ddca307c2f1c7573ae7087c486
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-10-19 21:44:22 +0200

    ejercicios

--- dia02/00_booleansYcondicionales.js
+++ dia02/00_booleansYcondicionales.js
@@ -19,3 +19,25 @@
 // OPERADOR TERNARIO
 
 // SWITCH
+
+console.log("1" + 1 + 1 + 1);
+console.log(1 + "1" + 1 + 1);
+console.log(1 + 1 + "1" + 1);
+console.log(1 + 1 + 1 + "1");
+console.log("3" - 3);
+console.log(4 + "3" - 2);
+console.log("3" * 3);
+console.log("10" / 2);
+console.log(10 + " objetos");
+console.log("8" * "8");
+let x = 2;
+console.log(3 - "x");
+console.log(3 - x);
+console.log(Boolean("Juan"));
+console.log(Boolean(""));
+let nombre = "";
+if (!nombre) {
+  console.log("no tienes nombre.");
+} else {
+  console.log(nombre);
+}
